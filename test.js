599739e11c4fcbac7596a9cd4b4edc7b1446d53e
Brian Woodward <brian.woodward@gmail.com>
2016-06-17 01:19:45 -0400

    tests

--- test.js
+++ test.js
@@ -2,27 +2,10 @@
 
 require('mocha');
 var assert = require('assert');
-var gulpGhClone = require('./');
+var clone = require('./');
 
 describe('gulp-gh-clone', function() {
   it('should export a function', function() {
-    assert.equal(typeof gulpGhClone, 'function');
-  });
-
-  it('should export an object', function() {
-    assert(gulpGhClone);
-    assert.equal(typeof gulpGhClone, 'object');
-  });
-
-  it('should throw an error when invalid args are passed', function(cb) {
-    try {
-      gulpGhClone();
-      cb(new Error('expected an error'));
-    } catch (err) {
-      assert(err);
-      assert.equal(err.message, 'expected first argument to be a string');
-      assert.equal(err.message, 'expected callback to be a function');
-      cb();
-    }
+    assert.equal(typeof clone, 'function');
   });
 });
